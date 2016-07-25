53ce28f40e9e517f4673797c438b1d878bed470a
Wilson Lu <huiquanlu@gmail.com>
2016-07-24 21:33:04 -0700

    fix logout and typo in login.js

--- config/fb.js
+++ config/fb.js
@@ -2,5 +2,5 @@
 module.exports = {
   'appID' : '644958238994099',
   'appSecret' : '7119012118bc1d93b115c2133b6273f0',
-  'callbackUrl' : 'http://localhost:3000/auth/facebook/callback'
+  'callbackUrl' : 'http://localhost:3000/users/auth/facebook/callback'
 }
--- passport/login.js
+++ passport/login.js
@@ -4,9 +4,55 @@ var bCrypt = require('bcrypt-nodejs');
 
 var fbConfig = require('../config/fb');
 var passport = require('passport');
-var FaceboookStrategy = require('passport-facebook').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function(passport){
+
+		passport.use('facebook', new FacebookStrategy({
+	  clientID        : fbConfig.appID,
+	  clientSecret    : fbConfig.appSecret,
+	  callbackURL     : fbConfig.callbackUrl,
+		profileFields: ['id', 'displayName', 'photos', 'email']
+	},
+	  // facebook will send back the tokens and profile
+	  function(access_token, refresh_token, profile, done) {
+	    // asynchronous
+	    process.nextTick(function() {
+	      // find the user in the database based on their facebook id
+	      User.findOne({ 'uid' : profile.id }, function(err, user) {
+	        // if there is an error, stop everything and return that
+	        // ie an error connecting to the database
+	        if (err)
+	          return done(err);
+	          // if the user is found, then log them in
+	          if (user) {
+	            return done(null, user); // user found, return that user
+	          } else {
+	            // if there is no user found with that facebook id, create them
+	            var newUser = new User();
+							console.log('Profile' + profile.displayName);
+	            // set all of the facebook information in our user model
+	            newUser.uid = profile.id; // set the users facebook id
+	            newUser.facebook_token = access_token; // we will save the token that facebook provides to the user
+	            newUser.firstName  = profile.name.givenName;
+	            newUser.lastName = profile.name.familyName; // look at the passport user profile to see how names are returned
+	            newUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+							newUser.provider = "facebook";
+	            // save our user to the database
+	            newUser.save(function(err) {
+	              if (err)
+	                throw err;
+	              // if successful, return the new user
+	              return done(null, newUser);
+	            });
+	         }
+	      });
+	    });
+	}));
+
+
+
+
 
 	passport.use('login', new LocalStrategy({
             usernameField: 'email',
