c45d62951a1eb46e9fd512cf66ab69b3f5f902b8
Wilson Lu <huiquanlu@gmail.com>
2016-07-30 18:37:24 -0700

    remove console.log

--- public/javascripts/linechart.js
+++ public/javascripts/linechart.js
@@ -1,13 +1,11 @@
 $(function () {
     var tableData = JSON.parse($('#chart').attr("value"));
     var tableName = $('#chart-name').attr("value");
-    console.log(tableData);
     // Format the multi-dimensional array into a displayable manner
     var seriesInput = tableData.slice(1, tableData.length-1).map(function(arr){
         return {name: arr[0], data: arr.slice(1,arr.length-1).map(function(elem){return elem / 1})}
     })
 
-    console.log(seriesInput);
 
     $('#chart').highcharts({
         title: {
