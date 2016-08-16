24a42164a1c105f9423edbf441f5da5ef2590512
Wilson Lu <huiquanlu@gmail.com>
2016-08-15 17:34:22 -0700

    production db

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://localhost/eCharts1'
+	'url' : 'mongodb://wilson:123@ds161475.mlab.com:61475/echartz'
+	// 'url' : 'mongodb://localhost/eCharts1'
 }
